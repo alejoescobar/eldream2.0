b8626b57f4517f4e41fc53980f3fb25f380be7f9
Alejandro Escobar <alejoescobac@gmail.com>
2014-01-21 16:55:11 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eldream20::Application.config.secret_token = 'c5a0e61c348ae2e25132fdd5afacf7c12c63f9c1671e520617aa38a2e63f64dd78118cd30685659470ff68132d93e790fcd157cb68b1ec6a7a2ea5e07d56e965'
